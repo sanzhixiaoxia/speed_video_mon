c6f62fa9a0a21ce7e7c8965931fcbd0b5c93a36d
wanglei21 <leiwang2010@163.com>
2023-12-15 15:22:54 +0800

    prod-1.6.6

--- all_net_speed_video_script.user.js
+++ all_net_speed_video_script.user.js
@@ -83,10 +83,8 @@
             .swal2-popup{font-family:"Arial",sans-serif;border-radius:10px;box-shadow:0 0 10px rgba(0,0,0,0.1);background-color:#fff;color:#333;}.swal2-title{font-size:24px;margin-bottom:10px;}
             .swal2-content{font-size:12px;margin-bottom:20px;}.swal2-actions{display:flex;justify-content:center;}.swal2-confirm,.swal2-cancel{font-size:18px;padding:10px 20px;border-radius:5px;background-color:#ffcc00;color:#fff;border:none;cursor:pointer;transition:background-color 0.3s;}.swal2-confirm:hover,.swal2-cancel:hover{background-color:#ffdd33;}
             .swal2-close{font-size:20px;width:30px;height:30px;line-height:30px;padding:0;border-radius:50%;background-color:#fff;color:#000;}
-            #switch_table>table{width:100%;border-collapse:collapse;}
-            #switch_table>table>tr,#switch_table>table>td{padding:10px;white-space:nowrap;}
-            #switch_table>table>th{background-color:#f5f5f5;font-weight:bold;}
-            #switch_table{border-collapse:separate;border-spacing:16px 0;}
+            #switch_table{border-collapse:separate;}#switch_table>table{width:100%;border-collapse:collapse;}
+            #switch_table>table>tr{white-space:nowrap;}#switch_table td{padding-right:22px;}#switch_table td:last-child{padding-right:0;}
             .message-content-sweetalert2{text-align:left;max-height:200px;overflow-y:auto;}
         `;
         GM_addStyle(customCss);
